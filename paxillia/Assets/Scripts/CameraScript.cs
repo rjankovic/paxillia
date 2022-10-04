9fabe8be3ae0df2d7fae47c51a82d849aa4394cc
Radovan Jankovic <jankovic.rj@gmail.com>
2022-10-04 18:37:42 +0200

    UI scaling

--- paxillia/Assets/Scripts/CameraScript.cs
+++ paxillia/Assets/Scripts/CameraScript.cs
@@ -8,6 +8,8 @@ public class CameraScript : MonoBehaviour
 
     [SerializeField]
     private Transform _targetPosition;
+    [SerializeField]
+    private bool _cameraMoves;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,65 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var newPos = new Vector3(_targetPosition.position.x, _targetPosition.position.y + 1f, -20f);
-        transform.position = Vector3.Slerp(transform.position, newPos, _movementSpeed * Time.deltaTime);
+        if (_cameraMoves)
+        {
+            var newPos = new Vector3(_targetPosition.position.x, _targetPosition.position.y + 1f, -20f);
+            transform.position = Vector3.Slerp(transform.position, newPos, _movementSpeed * Time.deltaTime);
+        }
+#if UNITY_EDITOR
+        if (_camera)
+            ScaleViewport();
+#endif
+    }
+
+    private Camera _camera;
+
+    //[Tooltip("Set the target aspect ratio.")]
+    //[SerializeField] 
+    
+    private float _targetAspectRatio = 16f/9f;
+
+    private void Awake()
+    {
+        _camera = GetComponent<Camera>();
+
+        if (Application.isPlaying)
+            ScaleViewport();
+    }
+
+
+    private void ScaleViewport()
+    {
+        // determine the game window's current aspect ratio
+        var windowaspect = Screen.width / (float)Screen.height;
+
+        // current viewport height should be scaled by this amount
+        var scaleheight = windowaspect / _targetAspectRatio;
+
+        // if scaled height is less than current height, add letterbox
+        if (scaleheight < 1)
+        {
+            var rect = _camera.rect;
+
+            rect.width = 1;
+            rect.height = scaleheight;
+            rect.x = 0;
+            rect.y = (1 - scaleheight) / 2;
+
+            _camera.rect = rect;
+        }
+        else // add pillarbox
+        {
+            var scalewidth = 1 / scaleheight;
+
+            var rect = _camera.rect;
+
+            rect.width = scalewidth;
+            rect.height = 1;
+            rect.x = (1 - scalewidth) / 2;
+            rect.y = 0;
+
+            _camera.rect = rect;
+        }
     }
 }
